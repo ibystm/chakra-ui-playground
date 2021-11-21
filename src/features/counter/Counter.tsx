572f80750c52f03f8ac07bfc4668d2f4a2101f1b
Kosuke Takahashi <rhcp.kdog@gmail.com>
2021-11-21 17:02:09 +0900

    styled changed.

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -13,9 +13,7 @@ export function Counter() {
   const count = useSelector((state) => counterValueSelecter(state));
   const dispatch = useAppDispatch();
   const [incrementAmount, setIncrementAmount] = useState("2");
-
   const incrementValue = Number(incrementAmount) || 0;
-
   const incrementWithAsync = () => {
     dispatch(incrementAsync(incrementValue));
   };
@@ -27,48 +25,63 @@ export function Counter() {
     <Box>
       <Flex align="center" justify="center" marginBottom={4}>
         <Button
-          fontSize={24}
+          fontSize="32px"
           color="rgb(112, 76, 182)"
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
-          marginLeft={3}
-          marginRight={3}
+          marginLeft={8}
+          marginRight={8}
+          h="80px"
+          w="80px"
         >
           -
         </Button>
-        <Box as="span">{count}</Box>
+        <Box as="span" fontSize="40px">
+          {count}
+        </Box>
         <Button
           color="rgb(112, 76, 182)"
-          fontSize={24}
+          h="80px"
+          w="80px"
+          marginLeft={8}
+          marginRight={8}
+          fontSize="32px"
           // className={styles.button}
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
-          marginLeft={3}
-          marginRight={3}
         >
           +
         </Button>
       </Flex>
       <Flex align="center" justify="center">
         <Input
-          w={12}
           aria-label="Set increment amount"
           value={incrementAmount}
+          h="80px"
+          w="80px"
+          textAlign="center"
+          fontSize="24px"
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
         <Button
           color="rgb(112, 76, 182)"
           onClick={substractWithAsync}
-          marginLeft={3}
-          marginRight={3}
+          marginLeft={4}
+          marginRight={4}
+          w="200px"
+          h="80px"
+          fontSize="24px"
         >
           Substract Async
         </Button>
         <Button
           color="rgb(112, 76, 182)"
           onClick={incrementWithAsync}
-          marginLeft={3}
-          marginRight={3}
+          marginLeft={4}
+          marginRight={4}
+          w="200px"
+          h="80px"
+          fontSize="24px"
         >
           Add Async
         </Button>
