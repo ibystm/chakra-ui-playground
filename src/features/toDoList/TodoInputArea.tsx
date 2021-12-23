c84782df1438b902c3169b9eb49aa6f88725970b
Kosuke Takahashi <rhcp.kdog@gmail.com>
2021-12-24 01:18:55 +0900

    modify reset all todo process.

--- src/features/toDoList/TodoInputArea.tsx
+++ src/features/toDoList/TodoInputArea.tsx
@@ -1,5 +1,17 @@
 import { RepeatIcon } from "@chakra-ui/icons";
-import { Box, Button, Input, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from "@chakra-ui/react";
 import React, { useCallback, useState } from "react";
 import { useDispatch } from "react-redux";
 import { MotionBox } from "../../components/Motions";
@@ -23,6 +35,9 @@ const TodoInputArea: React.FC = () => {
   const [input, setInput] = useState<string>("");
   const { errorObject, handleError } = useInputError();
   const { handlePressEnter } = useKeyboardEvents();
+  const [resetModalOpen, setResetModalOpen] = useState<boolean>(false);
+  const opneModal = () => setResetModalOpen(true);
+  const closeModal = () => setResetModalOpen(false);
 
   const dispatch = useDispatch();
   const createTodo = useCallback(() => {
@@ -44,41 +59,65 @@ const TodoInputArea: React.FC = () => {
     [handleError]
   );
 
-  const onClickReset = useCallback(() => {
+  const onClickReset = () => {
     dispatch(deleteAll());
-  }, [dispatch]);
+  };
+
+  const onPressAllDelete = () => {
+    onClickReset();
+    closeModal();
+  };
 
   return (
-    <MotionBox
-      w="320px"
-      variants={variants}
-      initial="hidden"
-      animate="visible"
-      display="flex"
-      justifyContent="space-between"
-    >
-      <Box width="75%">
-        <Input
-          aria-label="todo-input-area"
-          variant="flushed"
-          placeholder="New Todo"
-          size="lg"
-          focusBorderColor="purple.400"
-          onKeyDown={onPressEnter}
-          value={input}
-          onChange={handleChange}
-          isInvalid={errorObject.error}
-        />
-        {errorObject.message && (
-          <Text color="crimson" mt={2}>
-            {errorObject.message}
-          </Text>
-        )}
-      </Box>
-      <Button bg="none" onClick={onClickReset}>
-        <RepeatIcon />
-      </Button>
-    </MotionBox>
+    <>
+      <MotionBox
+        w="320px"
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        display="flex"
+        justifyContent="space-between"
+      >
+        <Box width="75%">
+          <Input
+            aria-label="todo-input-area"
+            variant="flushed"
+            placeholder="New Todo"
+            size="lg"
+            focusBorderColor="purple.400"
+            onKeyDown={onPressEnter}
+            value={input}
+            onChange={handleChange}
+            isInvalid={errorObject.error}
+          />
+          {errorObject.message && (
+            <Text color="crimson" mt={2}>
+              {errorObject.message}
+            </Text>
+          )}
+        </Box>
+        <Button bg="none" onClick={opneModal}>
+          <RepeatIcon />
+        </Button>
+      </MotionBox>
+      <Modal isOpen={resetModalOpen} onClose={closeModal}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Delete All Todo. </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Delete all todo. Is it Okay?</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={closeModal}>
+              Close
+            </Button>
+            <Button variant="ghost" onClick={onPressAllDelete}>
+              Delete
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
