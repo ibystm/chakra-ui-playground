e87b0e2a6a874cc904bc19b8bae61a5dea4099be
Kosuke Takahashi <rhcp.kdog@gmail.com>
2021-12-25 23:59:52 +0900

    add tsconfig for test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   setupFilesAfterEnv: ["<rootDir>/src/setUpTests.ts"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      tsconfig: "./tsconfig.test.json",
+    },
+  },
 };
